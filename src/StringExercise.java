cbf3218e3b3646f3d2cda67c25464ae94057b145
Jakira Dunson <jakiradunson@gmail.com>
2023-05-17 14:24:00 -0500

    arrays

---
+++ src/StringExercise.java
@@ -0,0 +1,24 @@
+public class StringExercise {
+    public static void main(String[] args){
+        //Question 1: String Basics
+
+      /*
+       String noEducation =  "We don't need no education";
+       String replaceString = noEducation.replace("education", "thought control");
+       System.out.println(noEducation);
+       System.out.println(replaceString);
+*/
+String noEducation = "We dont need no eduction\nWe don't need no tohught control";
+        System.out.println(noEducation);
+String checkOut = "Check \"this\" out!, \"s inside of \"s!\n";
+        System.out.println(checkOut);
+String windows = "In windows, the main drive is usually C:\\\n";
+        System.out.println(windows);
+String blackSlash = "I can do backslashes \\, double backslashes \\\\,\n" +
+        "and the amazing triple backslash \\\\\\!\n";
+        System.out.println(blackSlash);
+
+    }
+}
+
+
