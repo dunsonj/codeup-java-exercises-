90d658673072fca91950ddcfe0dc99cb40a8c0c1
Jakira Dunson <jakiradunson@gmail.com>
2023-05-23 10:01:50 -0500

    bonus

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -14,6 +14,4 @@ public class Circle {
         return Math.PI * radius * 2;
     }
 
-
-
 }
--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,4 +1,24 @@
 package shapes;
-
+import util.Input;
 public class CircleApp {
+    public static void main(String[] args) {
+        Input in = new Input ();
+        do {
+
+
+            System.out.println("Please enter a radius to receive the area and circumference of a circle");
+            double radius = in.getDouble();
+            Circle cir = new Circle(radius);
+
+            System.out.println("This is the area: " + cir.getArea());
+            System.out.println("This is the circumference " + cir.getCircumference());
+            System.out.println("Would you like to continue? ");
+
+            in.getString();
+        } while(in.yesNo());
+
+    }
+
+
+
 }
