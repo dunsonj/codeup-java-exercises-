bfd8bb23ad8d4ab6f76e035895b3c1c40da8696b
Jakira Dunson <jakiradunson@gmail.com>
2023-05-24 16:25:00 -0500

    shapes test

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -4,11 +4,10 @@ public class ShapesTest {
     public static void main(String[] args) {
         Rectangle box1 = new Rectangle(5, 4);
         System.out.println("Area: "+ box1.getArea());
-        System.out.println("Perimeter : "+ box1.getPerimeter());
+        System.out.println("Perimeter : "+ box1.getParameter());
 
-
-        Recatangle box2 = new Square(5);
+        Rectangle box2 = new Square(5);
         System.out.println("Area: "+ box2.getArea());
-        System.out.println("Perimeter : "+ box2.getPerimeter());
+        System.out.println("Perimeter : "+ box2.getParameter());
     }
 }
