19a1205accd38ec427893fa7cfc5b0f2a0162da4
Jakira Dunson <jakiradunson@gmail.com>
2023-05-24 16:10:05 -0500

    msquare

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -1,4 +1,14 @@
 package shapes;
 
 public class ShapesTest {
+    public static void main(String[] args) {
+        Rectangle box1 = new Rectangle(5, 4);
+        System.out.println("Area: "+ box1.getArea());
+        System.out.println("Perimeter : "+ box1.getPerimeter());
+
+
+        Recatangle box2 = new Square(5);
+        System.out.println("Area: "+ box2.getArea());
+        System.out.println("Perimeter : "+ box2.getPerimeter());
+    }
 }
