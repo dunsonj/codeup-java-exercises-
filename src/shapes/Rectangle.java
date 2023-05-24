2a833a16a0ec63d951a35b68f68a6e14e5988f82
Jakira Dunson <jakiradunson@gmail.com>
2023-05-24 16:18:52 -0500

    rectangle

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -2,10 +2,36 @@ package shapes;
 
 public class Rectangle {
     protected int length;
-
     protected int width;
+    public int getLength(){
+        return length;
+    }
+    public void setLength(int length){
+        this.length = length;
+    }
 
+    public int getWidth(){
+        return width;
+    }
+    public void setWidth(int width){
+        this.width = width;
+    }
+    public Rectangle(int length, int width){
+        this.length = length;
+        this.width = width;
+    }
+public int getArea(){
+        return length * width;
+}
+public int getParameter(){
+        return (2 * length) + (2 * width);
+}
 
+    public static void main(String[] args) {
+        Rectangle nRec = new Rectangle(5,6);
+        System.out.println(nRec.getArea());
+        System.out.println(nRec.getParameter());
+    }
 
 
 
--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,6 +1,9 @@
 package shapes;
 
 public class Square extends Rectangle{
+
+    protected int side;
+
     public Square(int side){
         super(side, side);
     }
