073dce9d60d0dc799f44aeff781868dc649aae19
Jakira Dunson <jakiradunson@gmail.com>
2023-05-24 10:34:28 -0500

    movie application-cleaned

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -1,5 +1,4 @@
 package movies;
-
 import util.Input;
 
 import static movies.MoviesArray.findAll;
@@ -13,58 +12,58 @@ public class MoviesApplication {
             System.out.println("Hello, Welcome to my movie application!");
             int choice = -1;
             while(choice !=0) {
-                System.out.println("What would you like to do?");
-                System.out.println("");
-                System.out.println("0 - exit");
-                System.out.println("1 - view all movies");
-                System.out.println("2 - view movies in the animated category");
-                System.out.println("3 - view movies in the drama category");
-                System.out.println("4 - view movies in the horror category");
-                System.out.println("5 - view movies in the scifi category");
-
-                System.out.println("Enter your choice: ");
+                printMenu();
 
                 choice = in.getInt(0, 5);
 
                 doChoice(choice);
             }
         }
-        private static void doChoice ( int choice){
-            switch (choice) {
-                case 1:
-                    for(Movie pMovie : mList){
-                        System.out.println(pMovie.getName() + "---" +pMovie.getCategory());
-                    }
-                    break;
-                case 2:
-                for(Movie pMovie : mList){
-                    if(pMovie.getCategory().equals("animated")){
-                        System.out.println(pMovie.getName() + "---" +pMovie.getCategory());
-                    }
+    private static void printMenu() {
+        System.out.println("What would you like to do?");
+        System.out.println("");
+        System.out.println("""
+                0 - exit");
+                1 - view all movies");
+                2 - view movies in the animated category
+                3 - view movies in the drama category
+                4 - view movies in the horror category
+                5 - view movies in the scifi category
+
+                Enter your choice: """);
+    }
+
+    private static void doChoice ( int choice) {
+        switch (choice) {
+            case 1:
+                for (Movie pMovie : mList) {
+                    System.out.println(pMovie.getName() + "---" + pMovie.getCategory());
                 }
                 break;
-                case 3:
-                    for(Movie pMovie : mList){
-                        if(pMovie.getCategory().equals("drama")){
-                            System.out.println(pMovie.getName() + "---" +pMovie.getCategory());
-                        }
-                    }
-                    break;
-                case 4:
-                    for(Movie pMovie : mList){
-                        if(pMovie.getCategory().equals("horror")){
-                            System.out.println(pMovie.getName() + "---" +pMovie.getCategory());
-                        }
-                    }
-                    break;
-                case 5:
-                    for(Movie pMovie : mList){
-                        if(pMovie.getCategory().equals("scifi")){
-                            System.out.println(pMovie.getName() + "---" +pMovie.getCategory());
-                        }
-                    }
-                    break;
+            case 2:
+                printMovies("animated");
+                break;
+            case 3:
+                printMovies("drama");
+                break;
+            case 4:
+                printMovies("horror");
+                break;
+            case 5:
+                printMovies("scifi");
+                break;
+        }
+    }
+
+        public static void printMovies (String category){
+            for (Movie pMovie : mList) {
+                if (pMovie.getCategory().equals(category) || category.equalsIgnoreCase("all")) {
+                    System.out.println(pMovie.getName() + "---" + pMovie.getCategory());
+                }
             }
         }
+
+
+
     }
 
