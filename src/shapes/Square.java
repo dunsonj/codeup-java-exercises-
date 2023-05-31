0c261fb9752e9d853765cf4e1682cc73d0935602
Jakira Dunson <jakiradunson@gmail.com>
2023-05-31 09:22:00 -0500

    square

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -1,5 +1,5 @@
 package shapes;
-
+/*
 public class Square extends Rectangle{
 
     protected int side;
@@ -18,5 +18,25 @@ public class Square extends Rectangle{
     public int getParameter() {
         System.out.println("This is the parameter: ");
         return 4 * side;
+    }
+}
+*/
+
+public class Square extends Quadrilateral{
+
+    protected double side;
+    @Override
+    public double getPerimeter() {
+        return 4 * side;
+    }
+
+    @Override
+    public double getArea() {
+        return side *side;
+    }
+
+    public Square(double side){
+        super(side,side);
+        this.side = side;
     }
 }
