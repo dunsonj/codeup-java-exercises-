635f9ec298c30cee55a9f7d8c66e4a214a1a6750
Jakira Dunson <jakiradunson@gmail.com>
2023-05-31 09:23:26 -0500

    rectangle

--- src/shapes/Rectangle.java
+++ src/shapes/Rectangle.java
@@ -1,5 +1,5 @@
 package shapes;
-
+/*
 public class Rectangle {
     protected int length;
     protected int width;
@@ -35,4 +35,23 @@ public int getParameter(){
 
 
 
+}
+*/
+
+public class Rectangle extends Quadrilateral {
+    protected double length;
+    protected double width;
+
+
+
+    @Override
+    public double getArea() {
+        return length* width;
+    }
+
+    public Rectangle(double length, double width){
+        super(length,width);
+        this.length = length;
+        this.width = width;
+    }
 }
